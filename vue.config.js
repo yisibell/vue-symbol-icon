1388b6f9da2e45aabaa2061c3c1f20b33716da24
wenqing <wenqing@kerrylan.com>
2023-12-05 11:40:49 +0800

    chore: using pnpm

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const path = require("path");
+const path = require('path')
 
-function resolve(dir) {
-  return path.join(__dirname, dir);
+function resolve (dir) {
+  return path.resolve(__dirname, dir)
 }
 
 module.exports = {
@@ -9,24 +9,24 @@ module.exports = {
     extract: false
   },
   productionSourceMap: false,
-  chainWebpack(config) {
+  chainWebpack (config) {
     // 变更 url-loader 不处理指定文件夹下作为icon使用的svg文件
     config.module
-      .rule("svg")
-      .exclude.add(resolve("src/icons"))
-      .end();
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
 
     // 添加 svg-sprite-loader 处理指定文件夹下的svg文件
     config.module
-      .rule("icons")
+      .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve("src/icons"))
+      .include.add(resolve('src/icons'))
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]"
+        symbolId: 'icon-[name]'
       })
-      .end();
+      .end()
   }
-};
+}
