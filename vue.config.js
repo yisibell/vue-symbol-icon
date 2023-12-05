d80b2ee11c1ad5199ac0fc9b731256e0fa643f75
wenqing <wenqing@kerrylan.com>
2023-12-05 11:31:38 +0800

    ci(example): add example build workflow

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,39 @@
-const path = require("path");
+const path = require('path')
 
-function resolve(dir) {
-  return path.join(__dirname, dir);
+function resolve (dir) {
+  return path.resolve(__dirname, dir)
+}
+
+const isBuildExample = () => {
+  const { BUILD_TYPE } = process.env
+  return BUILD_TYPE === 'example'
 }
 
 module.exports = {
+  publicPath: '/vue-symbol-icon/',
+  outputDir: isBuildExample ? 'example-dist' : 'dist',
   css: {
     extract: false
   },
   productionSourceMap: false,
-  chainWebpack(config) {
+  chainWebpack (config) {
     // 变更 url-loader 不处理指定文件夹下作为icon使用的svg文件
     config.module
-      .rule("svg")
-      .exclude.add(resolve("src/icons"))
-      .end();
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
 
     // 添加 svg-sprite-loader 处理指定文件夹下的svg文件
     config.module
-      .rule("icons")
+      .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve("src/icons"))
+      .include.add(resolve('src/icons'))
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]"
+        symbolId: 'icon-[name]'
       })
-      .end();
+      .end()
   }
-};
+}
